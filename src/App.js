c3bfbe95a4395a5c31ddb27737acdebbf6b01488
sinchanashettyl <sinchana.shetty@niveussolutions.com>
2022-10-11 14:16:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import About from './components/About';
+import Product from './components/Product'
+import Contact from './components/Contact';
+import {Navigate, Route, Routes} from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <Header/>
+      <Routes>
+        <Route to ="/" component={Home} />
+        <Route to="/products" component={Product} />
+        <Route to="/about" component={About} />
+        <Route to="/contact" component={Contact} />
+      
+        <Navigate to ="/" />
+      </Routes>
+      <Footer/>
+    </>
   );
 }
 
